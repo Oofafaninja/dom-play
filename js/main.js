5a907c6e0776f0792109f3c5c8f6c976a207b2ab
Brandon Tran <brandontran74@gmail.com>
2022-12-14 22:05:37 -0800

    final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-    //https://tinyurl.com/dynamic-html-checker
+
+    function highlightActor(actor){
+
+        let spans = document.querySelectorAll("#play span");
+        for(const mySpan of spans){
+
+        if(actor == mySpan.dataset.actor){// white, then turn yellow
+                mySpan.style.backgroundColor='yellow';
+        }else{// turn white
+                mySpan.style.backgroundColor='white';
+            }
+
+        }
+        
+    }
+
+    let spans = document.querySelectorAll("#play span");
+ 
+    console.log(spans);
+
+    for(const mySpan of spans){
+
+       mySpan.addEventListener("click", function(ev){
+            highlightActor(mySpan.dataset.actor);
+
+       });
+
+    }
+   
+   //https://tinyurl.com/dynamic-html-checker
     document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
       
     document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
@@ -12,7 +41,7 @@
         var x = document.getElementById("myTopnav");
         if (x.className === "topnav") {
             x.className += " responsive";
-        } else {
+        }else {
             x.className = "topnav";
         }
     }   
